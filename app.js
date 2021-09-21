d739f5e5aa6b5e7efccf8342262f3db24bae764b
Arpita Dasgupta <dasguptaarpita1999@gmail.com>
2021-09-21 11:00:18 +0530

    document.queryselector

---
+++ app.js
@@ -0,0 +1,8 @@
+var initialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var  currentPrice = document.querySelector("#current-price");
+
+var submitBtn =  document.querySelector("#submit-btn");
+var output = document.querySelector("#output-box");
+
+submitBtn.addEventListener('click', submitHandler)
